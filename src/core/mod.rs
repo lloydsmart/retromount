fc7b5a447b7daca7864e36b836450f72d7484a5c
Lloyd Smart <lloydsmart@users.noreply.github.com>
2026-03-11 13:24:06 +0000

    fix(core): typo in mod.rs and rename payload_filter

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -2,7 +2,7 @@ pub mod disc;
 pub mod game_image;
 pub mod input_handler;
 pub mod input_registry;
-pub mod payloa_filter;
+pub mod payload_filter;
 pub mod platform;
 pub mod reader;
 pub mod reader_factory;
